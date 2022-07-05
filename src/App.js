b682405db747c79370927c93f546119e7c5f4213
hormiguru <alfredo@caltum.com>
2022-07-05 11:24:10 -0500

    3.1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+// importamos el navBar
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const CartWidget = () => {
+    return (
+        <>
+            <i class="bi bi-cart"></i>
+        </>
+    )
+}
+export default CartWidget
