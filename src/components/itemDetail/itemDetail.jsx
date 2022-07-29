7e71046434647ca4d3a4370568ef26adbe9165b8
hormiguru <alfredo@caltum.com>
2022-07-28 21:38:26 -0500

    8.0

--- src/components/itemDetail/itemDetail.jsx
+++ src/components/itemDetail/itemDetail.jsx
@@ -1,57 +1,62 @@
-import React, { useEffect } from 'react'
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import React, { useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { getFetch } from '../../json/productos'
+import { useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { getFetch } from "../../json/productos";
 
-import ItemCount from '../../components/ItemCount/ItemCount'
+import ItemCount from "../../components/ItemCount/ItemCount";
 
-function ItemDetail({dataDetalle}) {
-const [producto,setProducto]=useState(false)
+function ItemDetail({ dataDetalle }) {
+  const [producto, setProducto] = useState(false);
+  const [isTrue,setIsTrue]=useState(true);
 
-const onAdd= (count) =>{
-  alert(`Compraste ${count} piezas`);
- }
-
-
-let{id}=useParams()
+  const onAdd = (count) => {
+    alert(`Compraste ${count} piezas`);
+    setIsTrue(false)
+    console.log(setIsTrue)
+  };
+  
+  let { id } = useParams();
   useEffect(() => {
-
     // getTask()
-
+    
     getFetch()
     // combierte productos en items
-      .then(res=>setProducto(res.filter(prod=>prod.id==id)))
-      .catch (error => console.log(error))
+    .then((res) => setProducto(res.filter((prod) => prod.id == id)))
+    .catch((error) => console.log(error));
     // return () => {
-
-    // }
-  }, [id])
-  // console.log(producto[0].name)
-  // 
+      
+      // }
+    }, [id]);
+    // console.log(producto[0].name)
+    //
   return (
     <div>
-    {producto&&producto.length>0?
-    <div>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={producto[0].imagen} />
-      <Card.Body>
-        <Card.Title> {producto[0].name} </Card.Title>
-        <Card.Text> {producto[0].detail} </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Precio ${producto[0].price}</ListGroup.Item>
-        <ListGroup.Item>inventario: {producto[0].stock} </ListGroup.Item>
-        <ItemCount initial={1} stock={producto[0].stock} onAdd={onAdd}/>
-      </ListGroup>
-    </Card>
-    
+      {producto && producto.length > 0 ? (
+        <div>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={producto[0].imagen} />
+            <Card.Body>
+              <Card.Title> {producto[0].name} </Card.Title>
+              <Card.Text> {producto[0].detail} </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              {isTrue ? <ItemCount initial={1} stock={producto[0].stock} onAdd={onAdd} />:
+              <Link to={'/cart'}>
+                <button>Ir al Carrito</button>
+              </Link>}
+              <ListGroup.Item>Precio ${producto[0].price}</ListGroup.Item>
+              <ListGroup.Item>inventario: {producto[0].stock} </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </div>
+      ) : (
+        ""
+        )}
     </div>
-    :""}
-  </div>
-  )
+  );
 }
 
-export default ItemDetail
+export default ItemDetail;
---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCrkE3wqBqo48b7mw46izUdiaQQk0KaVUs",
+    authDomain: "pet-store-d9a74.firebaseapp.com",
+    projectId: "pet-store-d9a74",
+    storageBucket: "pet-store-d9a74.appspot.com",
+    messagingSenderId: "449956088431",
+    appId: "1:449956088431:web:ece357e7ba868ce619db9a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// export const
