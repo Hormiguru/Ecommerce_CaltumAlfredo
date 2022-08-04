44793ca17db0df2a47e4c2546208981b7e682c1a
hormiguru <alfredo@caltum.com>
2022-08-03 23:27:34 -0500

    9.1

--- src/containers/cartContainer/cartContainer.jsx
+++ src/containers/cartContainer/cartContainer.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+// import React from 'react'
 import { useCartContext } from '../../context/CartContext'
 
-function CartContainer() {
+const CartContainer = () => {
   const {cartList} = useCartContext()
   return (
     <div>
